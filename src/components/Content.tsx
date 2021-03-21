a53ba426c5d1ec5967524a03e91deb6b875d4408
Rafael Renck <rafaelrenck@gmail.com>
2021-03-21 14:01:12 -0300

    done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+
+import { MovieCard } from './MovieCard';
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  genre: string;
+  movies: Movie[];
+};
+
+export function Content({genre, movies}: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {genre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
